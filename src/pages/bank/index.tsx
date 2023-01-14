1a24c15630e00966078974e215f9ea300ca50684
Ryan Martin <rmrt1n@users.noreply.github.com>
2023-01-15 03:45:08 +0800

    feat: add basic func for bank index page

--- src/pages/bank/index.tsx
+++ src/pages/bank/index.tsx
@@ -1,13 +1,36 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
+  const projects = [
+    {
+      id: 1,
+      name: 'C3 Cargos',
+      thumbnail: '/proj.jpg',
+    },
+    {
+      id: 2,
+      name: 'Aunty May Custom mugs',
+      thumbnail: '/cup.jpg',
+    },
+    {
+      id: 3,
+      name: 'Miniature trains',
+      thumbnail: '/train.jpg',
+    },
+  ]
   return (
-    <div className="space-y-4">
+    <div className="min-h-[calc(100vh-64px)] max-w-5xl p-4 lg:px-0 m-auto space-y-4 my-4">
       <h1 className="text-2xl font-bold">Projects List</h1>
       <div className="flex sm:grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-        { ['project-a', 'project-b', 'project-c'].map((proj) => (
-          <Link href={`/bank/projects/${proj}`} className="h-64 border rounded-lg shadow">
-            {proj}
+        { projects.map((proj) => (
+          <Link href={`/bank/projects/${proj.id}`} className="h-64 border rounded-lg shadow">
+          <div className="relative h-48">
+            <Image fill src={proj.thumbnail} alt="proj thumbnail" />
+          </div>
+          <div className="p-2">
+            <p className="font-bold">{proj.name}</p>
+          </div>
           </Link>
         ))}
       </div>
--- src/pages/bank/projects/[slug].tsx
+++ src/pages/bank/projects/[slug].tsx
@@ -1,12 +1,32 @@
 import { useRouter } from 'next/router';
 
+const projects = [
+  {
+    id: 1,
+    name: 'C3 Cargos',
+    thumbnail: '/proj.jpg',
+  },
+  {
+    id: 2,
+    name: 'Aunty May Custom mugs',
+    thumbnail: '/cup.jpg',
+  },
+  {
+    id: 3,
+    name: 'Miniature trains',
+    thumbnail: '/train.jpg',
+  },
+]
+
 export default function Project() {
   const router = useRouter()
-  const { slug } = router.query;
+  const { slug }: { slug: number } = router.query;
+  const project = projects[slug];
+  console.log(project)
 
   return (
     <>
-      this is the page for the { slug } project
+      this is the page for the {project.name}
     </>
   )
 }
